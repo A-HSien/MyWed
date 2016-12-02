752e6faab7b6d464177454a9579d37ac3541a61e
ahsien <ahsien@citystate.com.tw>
2016-12-03 02:46:11 +0800

    20161203

--- app.js
+++ app.js
@@ -261,35 +261,35 @@
     //// we'd only like to use iScroll for mobile...
     //if (false) {
     //    // configure iScroll
-    //    var myScroll = new IScroll('#wrapper',
-    //                {
-    //                    // don't scroll horizontal
-    //                    scrollX: false,
-    //                    // but do scroll vertical
-    //                    scrollY: true,
-    //                    // show scrollbars
-    //                    scrollbars: true,
-    //                    // deactivating -webkit-transform because pin wouldn't work because of a webkit bug: https://code.google.com/p/chromium/issues/detail?id=20574
-    //                    // if you dont use pinning, keep "useTransform" set to true, as it is far better in terms of performance.
-    //                    useTransform: false,
-    //                    // deativate css-transition to force requestAnimationFrame (implicit with probeType 3)
-    //                    useTransition: false,
-    //                    // set to highest probing level to get scroll events even during momentum and bounce
-    //                    // requires inclusion of iscroll-probe.js
-    //                    probeType: 3,
-    //                    // pass through clicks inside scroll container
-    //                    click: true
-    //                }
-    //            );
+    var myScroll = new IScroll('#wrapper',
+                {
+                    // don't scroll horizontal
+                    scrollX: false,
+                    // but do scroll vertical
+                    scrollY: true,
+                    // show scrollbars
+                    scrollbars: true,
+                    // deactivating -webkit-transform because pin wouldn't work because of a webkit bug: https://code.google.com/p/chromium/issues/detail?id=20574
+                    // if you dont use pinning, keep "useTransform" set to true, as it is far better in terms of performance.
+                    useTransform: false,
+                    // deativate css-transition to force requestAnimationFrame (implicit with probeType 3)
+                    useTransition: false,
+                    // set to highest probing level to get scroll events even during momentum and bounce
+                    // requires inclusion of iscroll-probe.js
+                    probeType: 3,
+                    // pass through clicks inside scroll container
+                    click: true
+                }
+            );
 
-    //    // overwrite scroll position calculation to use child's offset instead of container's scrollTop();
-    //    //scrollMagicController.scrollPos(function () {
-    //    //    return -myScroll.y;
-    //    //});
+    // overwrite scroll position calculation to use child's offset instead of container's scrollTop();
+    //scrollMagicController.scrollPos(function () {
+    //    return -myScroll.y;
+    //});
 
-    //    // thanks to iScroll 5 we now have a real onScroll event (with some performance drawbacks)
-    //    myScroll.on("scroll", function () {
-    //        scrollMagicController.update();
-    //    });
+    // thanks to iScroll 5 we now have a real onScroll event (with some performance drawbacks)
+    myScroll.on("scroll", function () {
+        scrollMagicController.update();
+    });
     //}
 });
