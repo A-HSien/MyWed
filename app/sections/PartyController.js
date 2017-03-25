ac0fc3695e703909d1e345e5546428f9a5fedeaa
ahsien <hsiaohsien0325@gmail.com>
2017-03-25 18:38:18 +0800

    201703251838

--- app/sections/PartyController.js
+++ app/sections/PartyController.js
@@ -1,11 +1,11 @@
 "use strict";
 var PartyController = (function () {
     function PartyController() {
-        $('#map-container').on('click', function (e) {
+        $('#map-container .map').on('click', function (e) {
             window.open('https://www.google.com.tw/maps/place/%E8%B1%AA%E9%BC%8E%E9%A3%AF%E5%BA%97+%E5%8C%97%E6%96%B0%E6%97%97%E8%89%A6%E9%A4%A8/@24.984038,121.5379173,17z/data=!3m1!4b1!4m5!3m4!1s0x346801fed70ad06d:0x2b2b0cae3db1b50c!8m2!3d24.984038!4d121.540106', '_blank');
         });
     }
     return PartyController;
 }());
 exports.PartyController = PartyController;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUGFydHlDb250cm9sbGVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiUGFydHlDb250cm9sbGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFFQTtJQUVJO1FBRUksQ0FBQyxDQUFDLGdCQUFnQixDQUFDLENBQUMsRUFBRSxDQUFDLE9BQU8sRUFBRSxVQUFBLENBQUM7WUFDN0IsTUFBTSxDQUFDLElBQUksQ0FBQywrT0FBK08sRUFBRSxRQUFRLENBQUMsQ0FBQztRQUMzUSxDQUFDLENBQUMsQ0FBQTtJQUNOLENBQUM7SUFDTCxzQkFBQztBQUFELENBQUMsQUFSRCxJQVFDO0FBUlksdUJBQWUsa0JBUTNCLENBQUEifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUGFydHlDb250cm9sbGVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiUGFydHlDb250cm9sbGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFFQTtJQUVJO1FBRUksQ0FBQyxDQUFDLHFCQUFxQixDQUFDLENBQUMsRUFBRSxDQUFDLE9BQU8sRUFBRSxVQUFBLENBQUM7WUFDbEMsTUFBTSxDQUFDLElBQUksQ0FBQywrT0FBK08sRUFBRSxRQUFRLENBQUMsQ0FBQztRQUMzUSxDQUFDLENBQUMsQ0FBQTtJQUNOLENBQUM7SUFDTCxzQkFBQztBQUFELENBQUMsQUFSRCxJQVFDO0FBUlksdUJBQWUsa0JBUTNCLENBQUEifQ==
--- app/sections/PartyController.ts
+++ app/sections/PartyController.ts
@@ -4,7 +4,7 @@ export class PartyController {
 
     constructor() {
 
-        $('#map-container').on('click', e => {
+        $('#map-container .map').on('click', e => {
             window.open('https://www.google.com.tw/maps/place/%E8%B1%AA%E9%BC%8E%E9%A3%AF%E5%BA%97+%E5%8C%97%E6%96%B0%E6%97%97%E8%89%A6%E9%A4%A8/@24.984038,121.5379173,17z/data=!3m1!4b1!4m5!3m4!1s0x346801fed70ad06d:0x2b2b0cae3db1b50c!8m2!3d24.984038!4d121.540106', '_blank');
         })
     }
